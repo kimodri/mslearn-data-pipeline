03b5a17250b7a70496100996a3a7978377427a30
Kim Audrey Magan <work.kimmagan@gmail.com>
2025-11-02 15:38:34 +0800

    Fixed the requirements

--- DeployResources/deploy-function-resources.ps1
+++ DeployResources/deploy-function-resources.ps1
@@ -1,28 +1,66 @@
-# Script creates the following Azure resources using Azure CLI commands:
-# Resource Group, Storage Account, Function App, App Service Plan (Consumption Plan), 
-# Application Insights (and an Action Group and Smart Detector Alert Rule used with Application Insights)
+$RG_NAME="data-pipeline-project"
+$SA_NAME="dailyfunctionstorage"
+$FUNCTION_APP_NAME="dailyjsonloaderfunc"
+$LOCATION="southeastasia"
+$FUNCTION_APP_CODE_PATH="C:\Users\magan\Desktop\ms-learn-data-pipeline\functions"
+
+# My blob storage
+$DATA_SA_NAME="mslearnjsonstorage"
+
+# create a storage account for my function app
+az storage account create `
+--name $SA_NAME `
+--location $LOCATION `
+--resource-group $RG_NAME `
+--sku Standard_LRS
+
+# create the function app that will hold my function in vscode (TIMERTRIGGER)
+Write-Host "Creating Function App..." -ForegroundColor Cyan
+az functionapp create `
+--name $FUNCTION_APP_NAME `
+--storage-account $SA_NAME `
+--consumption-plan-location $LOCATION `
+--resource-group $RG_NAME `
+--runtime python `
+--runtime-version 3.11 `
+--os-type Linux
+
+# Get the connection string
+$DATA_STORAGE_CONNECTION=$(az storage account show-connection-string `
+  --name $DATA_SA_NAME `
+  --resource-group $RG_NAME `
+  --query connectionString `
+  --output tsv)
+
+az functionapp config appsettings set `
+  --name $FUNCTION_APP_NAME `
+  --resource-group $RG_NAME `
+  --settings FUNCTIONS_WORKER_RUNTIME=python AzureWebJobsStorage="$DATA_STORAGE_CONNECTION" OutputStorageConnection="$DATA_STORAGE_CONNECTION"
+
+  az functionapp config appsettings list `
+  --name $FUNCTION_APP_NAME `
+  --resource-group $RG_NAME `
+  --output table
 
 
-# Resource Names and Location
-$ResourceGroupName = 'data-pipeline-project'
-$SA_NAME = "mslearnfuncstorage"
-$FUNCION_APP_NAME = 'dailyuploadfunc'
-$Location = 'southeastasia' # To see a list of all locations use: az account list-locations
+  # if you have other things add here also
 
-$DATA_SA_NAME = "mslearnjsonstorage"
+# zip the function in my codebase
+# Compress-Archive -Path $FUNCTION_APP_CODE_PATH\* -DestinationPath "$FUNCTION_APP_CODE_PATH\functionapp.zip"
+# az functionapp deployment source config-zip `
+# --name $FUNCTION_APP_NAME `
+# --resource-group $RG_NAME `
+# --src "$FUNCTION_APP_CODE_PATH\functionapp.zip"
 
-# Create resource group 
-az group create  --name $ResourceGroupName --location $Location
+# az functionapp deployment source config-zip `
+# --name $FUNCTION_APP_NAME `
+# --resource-group $RG_NAME `
+# --src "$FUNCTION_APP_CODE_PATH\functionapp.zip"
 
+# # verify
+# az function app config appsettings list `
+# --name $FUNCTION_APP_NAME `
+# --resource-group $RG_NAME `
+# --output table
 
-# Create general-purpose storage account
-az storage account create --name $StorageAccountName --location $Location --resource-group $ResourceGroupName --sku Standard_LRS
-
-
-# Create function app
-az functionapp create --resource-group $ResourceGroupName --consumption-plan-location $Location --runtime python --runtime-version 3.13 --functions-version 4 --name $FunctionAppName --os-type linux --storage-account $StorageAccountName
-
-$DATA_CONNECTION_STRING
-
-# To delete the resource group and all resources in it
-# az group delete --name rgmyfunc1 
+func azure functionapp publish $FUNCTION_APP_NAME
